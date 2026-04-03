1f716c3321882b20f8dd6e1f7b6ec5de168a4a6b
Achedon12 <leoderoinleo@gmail.com>
2026-04-03 18:36:31 +0200

    fix: matomo component

--- src/components/common/Matomo.jsx
+++ src/components/common/Matomo.jsx
@@ -1,20 +1,19 @@
-import { useEffect } from 'react'
+import {useEffect} from 'react'
 
 const Matomo = () => {
     useEffect(() => {
-        // Charger Matomo seulement en production
-        if (import.meta.env.VITE_APP_ENV === 'production' || import.meta.env.VITE_APP_ENV === 'prod' ) {
-            var _paq = window._paq = window._paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-                var u="//matomo.leoderoin.fr/";
-                _paq.push(['setTrackerUrl', u+'matomo.php']);
-                _paq.push(['setSiteId', '4']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-        }
+        var _paq = window._paq = window._paq || [];
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function () {
+            var u = "//matomo.leoderoin.fr/";
+            _paq.push(['setTrackerUrl', u + 'matomo.php']);
+            _paq.push(['setSiteId', '4']);
+            var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+            g.async = true;
+            g.src = u + 'matomo.js';
+            s.parentNode.insertBefore(g, s);
+        })();
     }, [])
 
     return null
